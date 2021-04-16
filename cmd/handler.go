5847dccb8fb0f16349234cf2d1bf777f416f8abc
Hong Jik  Lee <hankleeuva@gmail.com>
2021-04-16 03:10:09 -0400

    add handler to handle url path

---
+++ cmd/handler.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"log"           // New import
+    	"net/http"
+)
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "hello")
+}
+
+
+func home(w http.ResponseWriter, r *http.Request) {
+    if r.URL.Path != "/" {
+        http.NotFound(w, r)
+        return
+    }
+
+    // Initialize a slice containing the paths to the two files. Note that the
+    // home.page.tmpl file must be the *first* file in the slice.
+    files := []string{
+        "../ui/html/home.page.tmpl",
+        "../ui/html/base.layout.tmpl",
+	"../ui/html/footer.partial.tmpl",
+    }
+
+    // Use the template.ParseFiles() function to read the files and store the
+    // templates in a template set. Notice that we can pass the slice of file paths
+    // as a variadic parameter?
+    ts, err := template.ParseFiles(files...)
+    if err != nil {
+        log.Println(err.Error())
+        http.Error(w, "Internal Server Error", 500)
+        return
+    }
+
+    err = ts.Execute(w, nil)
+    if err != nil {
+        log.Println(err.Error())
+        http.Error(w, "Internal Server Error", 500)
+    }
+}
